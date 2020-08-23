5e8da2f2a09c16f33cfbf91a7a8fcee856501eae
Aleks <Aleks@AleKs>
2020-08-23 20:54:17 +0300

    Initial commit

--- DataFiterInfo/DataFiterInfo-ejb/src/java/project/postgreSQL/ConnectBeanLocal.java
+++ DataFiterInfo/DataFiterInfo-ejb/src/java/project/postgreSQL/ConnectBeanLocal.java
@@ -5,6 +5,11 @@ import javax.ejb.Local;
 
 
 @Local
-public interface ConnectBeanLocal {
+public interface ConnectBeanLocal  {
     
+    Integer[] getGruop();
+    Integer[] addGruop();
+    String[] getTeacherList();
+    String[] addTeacherList();
+   
 }
