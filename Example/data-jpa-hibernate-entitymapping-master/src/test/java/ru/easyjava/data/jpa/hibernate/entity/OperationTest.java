23f30e6282b3d35c19d613f68883ef907c19e9f5
Aleks <Aleks@AleKs>
2020-08-30 18:32:58 +0300

    Initial commit

---
+++ Example/data-jpa-hibernate-entitymapping-master/src/test/java/ru/easyjava/data/jpa/hibernate/entity/OperationTest.java
@@ -0,0 +1,44 @@
+package ru.easyjava.data.jpa.hibernate.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+public class OperationTest {
+    private EntityManagerFactory entityManagerFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        Operation op = new Operation();
+
+        op.setId(1L);
+        op.setAccountId(100500);
+        op.setAmount(BigDecimal.TEN);
+        op.setTimestamp(ZonedDateTime.now());
+        op.setDescription("Test operation");
+        op.setOpCode(9000);
+
+        entityManagerFactory = Persistence.createEntityManagerFactory("ru.easyjava.data.jpa.hibernate");
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(op);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Test
+    public void testGreeter() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.createQuery("from Operation", Operation.class)
+                .getResultList()
+                .forEach(System.out::println);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
