451cd711c4f532ae01cf0096e131402230450779
Aleks <Aleks@AleKs>
2020-09-09 19:53:44 +0300

    Initial commit

--- DataFiterInfo/DataFiterInfo-ejb/src/java/packet/ejb/service/ReadExcelData.java
+++ DataFiterInfo/DataFiterInfo-ejb/src/java/packet/ejb/service/ReadExcelData.java
@@ -3,6 +3,7 @@ package packet.ejb.service;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -19,6 +20,12 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcelData {
        
+     public static void main(String[] args) throws IOException {
+        
+        ReadExcelData pplb = new ReadExcelData();
+        pplb.extractExcelContentByColumnIndex(0);
+    }    
+    
       public List<String> extractExcelContentByColumnIndex(int columnIndex) {
         LinkedList<String> columndata = null;
        String fileName ="PrimerRaspisania.xlsx";
