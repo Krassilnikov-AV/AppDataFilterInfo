704c76d233c4f62a2bbdbe235e2bd98bd6bcf3e9
Aleks <Aleks@AleKs>
2020-09-10 21:36:34 +0300

    Initial commit

--- DataFiterInfo/DataFiterInfo-ejb/src/java/packet/ejb/service/HandlerBean.java
+++ DataFiterInfo/DataFiterInfo-ejb/src/java/packet/ejb/service/HandlerBean.java
@@ -4,22 +4,38 @@ package packet.ejb.service;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.Id;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
 import javax.sql.DataSource;
 
 /**
- *
+ * Класс HandlerBean
+ * - непосредственно взаимодействует с БД
+ * - переопределяет методы интерфейса HandlerBeanLocal
+ * 
  * @author Aleks
  */
-@Stateless
+@Entity
+@Table(name="raspisanie")
 @LocalBean
 public class HandlerBean implements HandlerBeanLocal {
 
   @Resource(name="5432/raspisanie")
+    @Id
    private DataSource ds;
    @PersistenceContext
     private EntityManager em;
+
+    public DataSource getDs() {
+        return ds;
+    }
+
+    public void setDs(DataSource ds) {
+        this.ds = ds;
+    }
    
    
 }
--- DataFiterInfo/DataFiterInfo-ejb/src/java/packet/ejb/service/HandlerBeanLocal.java
+++ DataFiterInfo/DataFiterInfo-ejb/src/java/packet/ejb/service/HandlerBeanLocal.java
@@ -1,10 +1,14 @@
 
 package packet.ejb.service;
 
+import javax.ejb.Local;
+
 /**
  * Интерфейс, который содержит основные методы для обработки данных
  * @author Aleks
  */
+
+@Local
 public interface HandlerBeanLocal {
     
 }
